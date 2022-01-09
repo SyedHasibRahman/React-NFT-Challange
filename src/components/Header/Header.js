2f32cf457ba03ebb825af0f61f431e8e479435ab
SyedHasibRahman <syedhasib01@gmail.com>
2022-01-09 17:07:12 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Header.css';
+import punkLogo from '../../assets/header/cryptopunk-logo.png';
+import searchIcon from '../../assets/header/search.png';
+import themeSwitcherIcon from '../../assets/header/theme-switch.png';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className="logoContainer">
+                <img src={ punkLogo } alt="" className='punkLogo' />
+            </div>
+            <div className="searchBar">
+                <div className="searchIconContainer">
+                    <img src={ searchIcon } alt="" />
+                </div>
+                <input className="searchInput" placeholder='Collection, item or user ...' />
+            </div>
+            <div className="headerItems">
+                <p>Drops</p>
+                <p>Marketplace</p>
+                <p>Create</p>
+            </div>
+
+            <div className="headerActions">
+                <div className="themeSwitcher">
+                    <img src={ themeSwitcherIcon } alt="" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
